4f78c822e9a2bb66204f15a084035593a5af9b6b
AdinaBechhofer <adinabec@gmail.com>
2020-10-19 00:54:28 -0400

    some bug fixes. Not fully tested yet

--- eval_f.m
+++ eval_f.m
@@ -8,11 +8,12 @@ d = p.Distance;
 phi = p.workFunction;
 Cec = p.CemitterCollector;
 Cp = p.Cparasitic;
+ROC = p.Radius;
 f = zeros(2*num_bowties, 1);
 for i =1:2*num_bowties
     if mod(i,2) ==0
-    f(i) = ((-x(i)/Re - A*jnano(beta*(x(i) - x(i+1))/d) +u(i))/(Cec+Cp) +(Cec/(Cec+Cp)^2)*(-x(i+1)/Rc +A*jnano(beta*(x(i) - x(i+1))/d)+u(i+1)))/(1-Cec^2/(Cec+cp)^2);
+    f(i) = ((-x(i)/Re - A*jnano(phi,beta*(x(i) - x(i+1))/d, ROC) +u(i))/(Cec+Cp) +(Cec/(Cec+Cp)^2)*(-x(i+1)/Rc +A*jnano(phi,beta*(x(i) - x(i+1))/d, ROC)+u(i+1)))/(1-Cec^2/(Cec+cp)^2);
     else
-    f(i) = ((-x(i)/Rc + A*jnano(beta*(x(i-1) - x(i))/d) +u(i))/(Cec+Cp) +(Cec/(Cec+Cp)^2)*(-x(i-1)/Re -A*jnano(beta*(x(i-1) - x(i))/d)+u(i-1)))/(1-Cec^2/(Cec+cp)^2);
+    f(i) = ((-x(i)/Rc + A*jnano(phi,beta*(x(i-1) - x(i))/d, ROC) +u(i))/(Cec+Cp) +(Cec/(Cec+Cp)^2)*(-x(i-1)/Re -A*jnano(phi,beta*(x(i-1) - x(i))/d, ROC)+u(i-1)))/(1-Cec^2/(Cec+cp)^2);
     end 
 end 
