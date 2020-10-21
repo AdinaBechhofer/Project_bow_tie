b46b2a7a8da0ec0d7a7c7463f84a35f1eb7b1cea
AdinaBechhofer <adinabec@gmail.com>
2020-10-21 11:05:40 -0400

    updated

--- test_eval_f.m
+++ test_eval_f.m
@@ -1,25 +1,25 @@
 p.NumBowties = 50;
-p.REmitter = 0.1; % 1 ohm 
-p.RCollector = 0.1; % 1 ohm 
+p.REmitter = 1; % 1 ohm 
+p.RCollector = 1; % 1 ohm 
 p.Area = 0.0001; % need to actually get a reasonable estimate  
 p.Beta = 0.9; % enhancement factor 
 p.Distance = 10; % 10 nm
 p.workFunction = 5.1; % work function of gold 
-p.CemitterCollector = 0.001; % nano farad
-p.Cparasitic = 0.0001; % 0.1 nano farad
+p.CemitterCollector = 0.1; % nano farad
+p.Cparasitic = 0.1; % 0.1 nano farad
 p.Radius = 1; % 1 nm?
 p.taby = csvread('rspa20140811supp3.csv');    
 
 v1 = 5;
-v2 = 0.2;
+v2 = 0;
 x0 = zeros(2*p.NumBowties, 1);
 u0 = zeros(2*p.NumBowties, 1);
 u0(1:2:end) = v1/p.REmitter;
 u0(2:2:end) = v2/p.RCollector;
 
-t_stop = 0.1;
+t_stop = 1;
 t_start = 0;
-timestep = 0.0005;
+timestep = 0.05;
 
 % Implement forward euler
 X = ForwardEulerNew(x0,p,u0,t_start,t_stop,timestep, p.taby);
