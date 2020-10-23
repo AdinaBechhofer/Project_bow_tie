37386c7e2089987a6fc5708ab7c64cb5ae5d63dc
AdinaBechhofer <adinabec@gmail.com>
2020-10-22 20:26:15 -0400

    added off diagonal terms

--- test_eval_f.m
+++ test_eval_f.m
@@ -9,7 +9,9 @@ p.CemitterCollector = 0.1; % nano farad
 p.Cparasitic = 0.08; % 0.1 nano farad
 p.Radius = 1; % 1 nm?
 p.taby = csvread('rspa20140811supp3.csv');    
-
+p.row = 5;
+p.col = 10;
+p.Ccoupling = 0.03;
 u.jnano = 1;
 
 v1 = 5;
@@ -23,11 +25,19 @@ G = zeros(2*p.NumBowties); % if G is non-linear need to stamp inside eval_f
 for i = 1:2*p.NumBowties
     C(i,i) = p.CemitterCollector+p.Cparasitic;
     if mod(i,2) == 1
-        C(i,i+1)= -p.CemitterCollector;
-        G(i,i) = -1./(p.REmitter);
+        C(i,i+1)= C(i,i+1)-p.CemitterCollector;
+        G(i,i) = G(i,i) -1./(p.REmitter);
+        if  i> 2*p.col
+            C(i,i) = C(i,i) + p.Ccoupling;
+            C(i,i-2*p.col+1) = C(i,i-2*p.col+1) -p.Ccoupling;
+        end 
     else
         C(i,i-1)= -p.CemitterCollector;
         G(i,i) = -1./(p.RCollector);
+        if i<= 2*p.col*(p.row-1)
+            G(i,i) = G(i,i) + p.Ccoupling;
+            G(i,i+2*p.col-1) = G(i,i+2*p.col-1) + p.Ccoupling;
+        end
     end
 end
 invC = inv(C);
@@ -37,7 +47,7 @@ x0 = zeros(2*p.NumBowties, 1);
 
 t_stop = 1;
 t_start = 0;
-timestep = 0.05;
+timestep = 0.005;
 
 % Implement forward euler
 X = ForwardEulerNew(x0,p,u,t_start,t_stop,timestep);
@@ -46,8 +56,11 @@ figure;
 plot(X(5,:))
 hold on;
 plot(X(6,:))
-
-legend('vodd','veven')
+plot(X(5+4*p.col,:))
+plot(X(6+4*p.col,:))
+plot(X(5+2*(p.row-1)*p.col, :))
+plot(X(6+2*(p.row-1)*p.col, :))
+legend('vodd1','veven1','vodd2','veven2','vodd3','veven3')
 title('voltages over time','FontSize', 8)
 hold off;
 
